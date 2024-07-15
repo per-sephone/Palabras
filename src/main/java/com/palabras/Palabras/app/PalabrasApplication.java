b57bef298f4f73ba95f228d843fc6748a0fb1624
Nora Luna <nora.luna@hp.com>
2024-07-15 12:56:36 -0700

    updates

--- src/main/java/com/palabras/Palabras/app/PalabrasApplication.java
+++ src/main/java/com/palabras/Palabras/app/PalabrasApplication.java
@@ -3,8 +3,9 @@ package com.palabras.Palabras.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableMongoRepositories("com.palabras.Palabras.model")
 public class PalabrasApplication {
 
